b589eab714486b850848f557763227dbe31b6bde
UMINATI <uminatililing654@gmail.com>
2026-07-24 18:07:23 +0800

    Add files via upload

---
+++ 01_create_database.sql
@@ -0,0 +1,8 @@
+-- =====================================================================
+-- 01. CREATE DATABASE
+-- Sistem Basis Data Skripsi/Tugas Akhir Mahasiswa - Kelompok 11
+-- =====================================================================
+
+DROP DATABASE IF EXISTS db_skripsi_mahasiswa;
+CREATE DATABASE db_skripsi_mahasiswa;
+USE db_skripsi_mahasiswa;
